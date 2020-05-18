004217d2a2684733813b23f2f5d79eb5f5451d3e
Maximilian Laue <Maximilian.Laue@t-systems.com>
2020-05-18 14:52:54 +0200

    style: removed unused imports

--- src/main/java/app/coronawarn/verification/services/controller/VerificationController.java
+++ src/main/java/app/coronawarn/verification/services/controller/VerificationController.java
@@ -36,8 +36,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**

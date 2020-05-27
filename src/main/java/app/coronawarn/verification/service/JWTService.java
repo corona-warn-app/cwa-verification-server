ba649c0ce85ca4082cf7511d15aa770b7b70fc4f
Lars Stelzner <lars.stelzner@t-systems.com>
2020-05-27 15:56:56 +0200

    JWTService implemetation for validate the JW-Token.

--- src/main/java/app/coronawarn/verification/service/JWTService.java
+++ src/main/java/app/coronawarn/verification/service/JWTService.java
@@ -1,18 +1,24 @@
-/* 
- * Copyright 2020 T-Systems International GmbH.
+/*
+ * Corona-Warn-App / cwa-verification
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * (C) 2020, T-Systems International GmbH
+ *
+ * Deutsche Telekom AG and all other contributors /
+ * copyright owners license this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package app.coronawarn.verification.service;
 
 import io.jsonwebtoken.Claims;
@@ -32,16 +38,16 @@ public class JWTService implements Serializable {
     return (roleName.equals(getRoleNameFromToken(token)));
   }
 
-  public String getRoleNameFromToken(String token) {
+  public String getRoleNameFromToken(final String token) {
     return getClaimFromToken(token, Claims::getSubject);
   }
 
-  public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+  public <T> T getClaimFromToken(final String token, Function<Claims, T> claimsResolver) {
     final Claims claims = getAllClaimsFromToken(token);
     return claimsResolver.apply(claims);
   }
 
-  private Claims getAllClaimsFromToken(String token) {
+  private Claims getAllClaimsFromToken(final String token) {
     return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
   }
 

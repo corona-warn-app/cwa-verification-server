e253ce8e2b518aae79d9db13ccfb5b21118cd076
Martin Müller-Rohde <mmr@scoop-software.de>
2020-05-28 09:20:45 +0200

    add a simple filter which prevents BSI issue #55 (Missing request size limitation leads to memory exhaustion)

---
+++ src/main/java/app/coronawarn/verification/config/PostSizeLimitFilter.java
@@ -0,0 +1,38 @@
+package app.coronawarn.verification.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class PostSizeLimitFilter extends OncePerRequestFilter {
+
+  @Value("${server.max-post-size:10000}")
+  private long maxPostSize;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request,
+                                  HttpServletResponse response, FilterChain filterChain)
+    throws ServletException, IOException {
+    if (isPOST(request) && request.getContentLengthLong() > maxPostSize) {
+      response.setStatus(HttpStatus.NOT_ACCEPTABLE.value());
+      return;
+    }
+    filterChain.doFilter(request, response);
+  }
+
+  private boolean isPOST(HttpServletRequest httpRequest) {
+    return HttpMethod.POST.matches(httpRequest.getMethod());
+  }
+
+}

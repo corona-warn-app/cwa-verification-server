a48ebb0afe4ee19c19603f8895547b955ecfd535
dfischer <65018958+dfischer-tech@users.noreply.github.com>
2020-05-31 12:04:58 +0200

    Update HashedGuid.java

--- src/main/java/app/coronawarn/verification/model/HashedGuid.java
+++ src/main/java/app/coronawarn/verification/model/HashedGuid.java
@@ -26,6 +26,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
+ * This class represents the hashed Guid.
  * @see <a href="https://github.com/corona-warn-app/cwa-testresult-server/blob/master/docs/architecture-overview.md#core-entities">Core Entities</a>
  */
 @Data

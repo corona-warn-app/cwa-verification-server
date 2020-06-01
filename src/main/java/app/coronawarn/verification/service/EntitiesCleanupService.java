34d9f96ee3c0723a95bb9704cd8cbb0bf0669615
mschulte-tsi <65006436+mschulte-tsi@users.noreply.github.com>
2020-06-01 13:29:23 +0200

    Fix cleanup (#115)
    * fix:cleanup
    
    * fix: codestyle
    
    * fix: documentation
    
    * fix: checkstyle
    
    * fix: doku
    
    * fix: checkstyle
    
    * fix: codesmells
    
    * fix: codestyle
    
    * fix: return type
    
    * fix: updated default values
    
    * fix: cleanupDatabaseTest

--- src/main/java/app/coronawarn/verification/service/EntitiesCleanupService.java
+++ src/main/java/app/coronawarn/verification/service/EntitiesCleanupService.java
@@ -32,6 +32,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+/**
+ * A Service to delete entities that are older than configured days.
+ */
 @Slf4j
 @RequiredArgsConstructor
 @Component

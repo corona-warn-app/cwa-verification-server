46055abd829a6a16e86bd41540d568368b78afcd
dfischer <65018958+dfischer-tech@users.noreply.github.com>
2020-06-03 07:59:50 +0200

    feat: spring security integration and logging (#123)
    * feat: spring security integration
    
    * feat: logging

--- src/main/java/app/coronawarn/verification/VerificationApplication.java
+++ src/main/java/app/coronawarn/verification/VerificationApplication.java
@@ -27,12 +27,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * The Application class.
  */
 @EnableFeignClients
 @SpringBootApplication
+@EnableWebSecurity
 @EnableScheduling
 @EnableConfigurationProperties({VerificationApplicationConfig.class})
 public class VerificationApplication {

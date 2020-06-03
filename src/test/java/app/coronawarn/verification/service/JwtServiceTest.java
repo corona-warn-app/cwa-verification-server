491ec47c9b238dde860219b7a897737491aa35e5
lstelzne-tech <lars.stelzner@t-systems.com>
2020-06-03 20:20:52 +0200

    After refactoring the role-enum to AuthoriaztionRole by mlaue, I adjust and correct the junit-classes too.

--- src/test/java/app/coronawarn/verification/service/JwtServiceTest.java
+++ src/test/java/app/coronawarn/verification/service/JwtServiceTest.java
@@ -23,6 +23,7 @@ package app.coronawarn.verification.service;
 
 import app.coronawarn.verification.VerificationApplication;
 import app.coronawarn.verification.model.AuthorizationRole;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.io.UnsupportedEncodingException;
@@ -54,9 +55,15 @@ public class JwtServiceTest {
    * @throws java.io.UnsupportedEncodingException
    */
   @Test
-  public void testValidateToken() throws UnsupportedEncodingException {
-    String jwToken = getJwtTestData(3000, AuthorizationRole.AUTH_C19_HOTLINE, AuthorizationRole.AUTH_C19_HEALTHAUTHORITY);
-    Assert.assertTrue(jwTService.isAuthorized(JwtService.TOKEN_PREFIX + jwToken));
+  public void testValidToken() throws UnsupportedEncodingException {
+    String jwToken = getJwtTestData(120, AuthorizationRole.AUTH_C19_HOTLINE, AuthorizationRole.AUTH_C19_HEALTHAUTHORITY);
+    Assert.assertTrue(jwTService.validateToken(jwToken));
+  }
+
+  @Test
+  public void testExpiredToken() throws UnsupportedEncodingException {
+    String jwToken = getJwtTestData(0, AuthorizationRole.AUTH_C19_HOTLINE);
+    Assert.assertFalse(jwTService.validateToken(jwToken));
   }
 
   private String getJwtTestData(final long expirationSecondsToAdd, AuthorizationRole... roles) throws UnsupportedEncodingException {
@@ -70,25 +77,25 @@ public class JwtServiceTest {
     realm_accessMap.put("roles", roleNames);
 
     return Jwts.builder()
-      .setExpiration(Date.from(Instant.now().plusSeconds(expirationSecondsToAdd)))
-      .setIssuedAt(Date.from(Instant.now()))
-      .setId("baeaa733-521e-4d2e-8abe-95bb440a9f5f")
-      .setIssuer("http://localhost:8080/auth/realms/cwa")
-      .setAudience("account")
-      .setSubject("72b3b494-a0f4-49f5-b235-1e9f93c86e58")
-      .claim("auth_time", "1590742669")
-      .claim("iss", "http://localhost:8080/auth/realms/cwa")
-      .claim("aud", "account")
-      .claim("typ", "Bearer")
-      .claim("azp", "verification-portal")
-      .claim("session_state", "41cc4d83-e394-4d08-b887-28d8c5372d4a")
-      .claim("acr", "0")
-      .claim("realm_access", realm_accessMap)
-      .claim("resource_access", new HashMap())
-      .claim("scope", "openid profile email")
-      .claim("email_verified", false)
-      .claim("preferred_username", "test")
-      .signWith(SignatureAlgorithm.HS256, secret.getBytes("UTF-8"))
-      .compact();
+            .setExpiration(Date.from(Instant.now().plusSeconds(expirationSecondsToAdd)))
+            .setIssuedAt(Date.from(Instant.now()))
+            .setId("baeaa733-521e-4d2e-8abe-95bb440a9f5f")
+            .setIssuer("http://localhost:8080/auth/realms/cwa")
+            .setAudience("account")
+            .setSubject("72b3b494-a0f4-49f5-b235-1e9f93c86e58")
+            .claim("auth_time", "1590742669")
+            .claim("iss", "http://localhost:8080/auth/realms/cwa")
+            .claim("aud", "account")
+            .claim("typ", "Bearer")
+            .claim("azp", "verification-portal")
+            .claim("session_state", "41cc4d83-e394-4d08-b887-28d8c5372d4a")
+            .claim("acr", "0")
+            .claim("realm_access", realm_accessMap)
+            .claim("resource_access", new HashMap())
+            .claim("scope", "openid profile email")
+            .claim("email_verified", false)
+            .claim("preferred_username", "test")
+            .signWith(SignatureAlgorithm.HS256, secret.getBytes("UTF-8"))
+            .compact();
   }
 }

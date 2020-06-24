6983aa6da521b777cf7cfbcc3a95742bec63badb
dfischer <65018958+dfischer-tech@users.noreply.github.com>
2020-06-24 11:02:50 +0200

    Feat/improvement logs regarding cdc (#155)
    feat: added logging for TAN creation and test result acquisition

--- src/main/java/app/coronawarn/verification/controller/VerificationController.java
+++ src/main/java/app/coronawarn/verification/controller/VerificationController.java
@@ -192,9 +192,10 @@ public class VerificationController {
             throw new VerificationServerException(HttpStatus.BAD_REQUEST,
               "Unknown source of trust inside the appsession for the registration token");
         }
-        String generatedTan = tanService.generateVerificationTan(tanSourceOfTrust);
         appSession.incrementTanCounter();
         appSessionService.saveAppSession(appSession);
+        String generatedTan = tanService.generateVerificationTan(tanSourceOfTrust);
+        log.info("Returning the successfully generated tan.");
         return ResponseEntity.status(HttpStatus.CREATED).body(new Tan(generatedTan));
       }
       throw new VerificationServerException(HttpStatus.BAD_REQUEST,
@@ -228,11 +229,13 @@ public class VerificationController {
       appSessionService.getAppSessionByToken(registrationToken.getRegistrationToken());
     if (appSession.isPresent()) {
       if ((appSession.get().getHashedGuid() == null) && (appSession.get().getTeleTanHash() != null)) {
+        log.info("The result will be returned.");
         return ResponseEntity.ok(new TestResult(LabTestResult.POSITIVE.getTestResult()));
       }
       String hash = appSession.get().getHashedGuid();
       log.info("Requested result for registration token with hashed Guid.");
       TestResult testResult = testResultServerService.result(new HashedGuid(hash));
+      log.info("The result will be returned.");
       return ResponseEntity.ok(testResult);
     }
     log.info("The registration token doesn't exists.");
@@ -261,12 +264,13 @@ public class VerificationController {
       .filter(t -> t.canBeRedeemed(LocalDateTime.now()))
       .map(t -> {
         tanService.deleteTan(t);
+        log.info("The Tan is valid.");
         return t;
       })
       .map(t -> ResponseEntity.ok().build())
       .orElseGet(() -> {
         log.info("The Tan is invalid.");
-        throw new VerificationServerException(HttpStatus.NOT_FOUND, "No Tan found");
+        throw new VerificationServerException(HttpStatus.NOT_FOUND, "No Tan found or Tan is invalid");
       });
   }
 

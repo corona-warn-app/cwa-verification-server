9c49714c73d965c364f0afef4846a6e822897561
Michael Schulte <65006436+mschulte-tsi@users.noreply.github.com>
2020-07-21 16:21:39 +0200

    Update src/main/java/app/coronawarn/verification/controller/ExternalTokenController.java
    Co-authored-by: Felix Dittrich <31076102+f11h@users.noreply.github.com>

--- src/main/java/app/coronawarn/verification/controller/ExternalTokenController.java
+++ src/main/java/app/coronawarn/verification/controller/ExternalTokenController.java
@@ -66,6 +66,7 @@ public class ExternalTokenController {
 
   /**
    * This method generates a registrationToken by a hashed guid or a teleTAN.
+   
    * @param request {@link RegistrationTokenRequest}
    * @return RegistrationToken - the created registration token {@link RegistrationToken}
    */

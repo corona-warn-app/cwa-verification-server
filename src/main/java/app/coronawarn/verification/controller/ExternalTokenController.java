35691f8da4264a4beb8aeb3e55acb48eabe6cf27
Damir Seit <damir.seit.00@gmail.com>
2022-02-28 10:37:17 +0600

    misspelled username

--- src/main/java/app/coronawarn/verification/controller/ExternalTokenController.java
+++ src/main/java/app/coronawarn/verification/controller/ExternalTokenController.java
@@ -20,12 +20,10 @@
 
 package app.coronawarn.verification.controller;
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-
-import app.coronawarn.verification.domain.VerificationTan;
-import app.coronawarn.verification.exception.VerificationServerException;
+import app.coronawarn.verification.covid_test_identifier_factory.COVIDTestIdentifier;
+import app.coronawarn.verification.covid_test_identifier_factory.COVIDTestIdentifierFactory;
+import app.coronawarn.verification.covid_test_identifier_factory.COVIDTestIdentifierFactoryImpl;
 import app.coronawarn.verification.model.RegistrationToken;
-import app.coronawarn.verification.model.RegistrationTokenKeyType;
 import app.coronawarn.verification.model.RegistrationTokenRequest;
 import app.coronawarn.verification.service.AppSessionService;
 import app.coronawarn.verification.service.FakeDelayService;
@@ -34,14 +32,13 @@ import app.coronawarn.verification.service.TanService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import java.util.Optional;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
@@ -103,44 +100,11 @@ public class ExternalTokenController {
     }
     StopWatch stopWatch = new StopWatch();
     stopWatch.start();
-    String key = request.getKey();
-    RegistrationTokenKeyType keyType = request.getKeyType();
-    DeferredResult<ResponseEntity<RegistrationToken>> deferredResult = new DeferredResult<>();
 
-    switch (keyType) {
-      case GUID:
-        ResponseEntity<RegistrationToken> responseEntity =
-          appSessionService.generateRegistrationTokenByGuid(key, request.getKeyDob(), fake);
-        stopWatch.stop();
-        fakeDelayService.updateFakeTokenRequestDelay(stopWatch.getTotalTimeMillis());
-        deferredResult.setResult(responseEntity);
-        log.info("Returning the successfully generated RegistrationToken.");
-        return deferredResult;
-      case TELETAN:
-        Optional<VerificationTan> optional = tanService.getEntityByTan(key);
+    COVIDTestIdentifierFactory covidTestIdentifierFactory = new COVIDTestIdentifierFactoryImpl();
 
-        ResponseEntity<RegistrationToken> response = appSessionService.generateRegistrationTokenByTeleTan(
-          key,
-          fake,
-          optional.map(VerificationTan::getTeleTanType).orElse(null));
+    COVIDTestIdentifier testIdentifier = covidTestIdentifierFactory.makeCOVIDTestIdentifier(request);
 
-        if (optional.isPresent()) {
-          VerificationTan teleTan = optional.get();
-          teleTan.setRedeemed(true);
-          tanService.saveTan(teleTan);
-          stopWatch.stop();
-          fakeDelayService.updateFakeTokenRequestDelay(stopWatch.getTotalTimeMillis());
-          scheduledExecutor.schedule(() -> deferredResult.setResult(response), fakeDelayService.realDelayToken(),
-            MILLISECONDS);
-          log.info("Returning the successfully generated RegistrationToken.");
-          return deferredResult;
-        }
-        stopWatch.stop();
-        throw new VerificationServerException(HttpStatus.BAD_REQUEST, "The teleTAN verification failed");
-      default:
-        stopWatch.stop();
-        throw new VerificationServerException(HttpStatus.BAD_REQUEST,
-          "Unknown registration key type for registration token");
-    }
+    return testIdentifier.generateRegistrationToken(request, scheduledExecutor, stopWatch, fake, appSessionService, fakeDelayService, tanService);
   }
 }

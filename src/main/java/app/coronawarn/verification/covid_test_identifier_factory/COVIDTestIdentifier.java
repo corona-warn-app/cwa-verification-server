3089f1d1a2792d8cda5f57b0cd6727f59160d767
Damir Seit <damir.seit.00@gmail.com>
2022-02-28 10:39:38 +0600

    Registration Token Refactor (Abstract Factory)

---
+++ src/main/java/app/coronawarn/verification/covid_test_identifier_factory/COVIDTestIdentifier.java
@@ -0,0 +1,36 @@
+/*-
+ * ---license-start
+ * Corona-Warn-App / cwa-verification
+ * ---
+ * Copyright (C) 2020 - 2022 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
+package app.coronawarn.verification.covid_test_identifier_factory;
+
+import app.coronawarn.verification.model.RegistrationToken;
+import app.coronawarn.verification.model.RegistrationTokenRequest;
+import app.coronawarn.verification.service.AppSessionService;
+import app.coronawarn.verification.service.FakeDelayService;
+import app.coronawarn.verification.service.TanService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+public abstract class COVIDTestIdentifier {
+  public abstract DeferredResult<ResponseEntity<RegistrationToken>> generateRegistrationToken(RegistrationTokenRequest request, ScheduledExecutorService scheduledExecutor, StopWatch stopWatch, String fake, AppSessionService appSessionService, FakeDelayService fakeDelayService, TanService tanService);
+}
